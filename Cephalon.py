fee6782b1e1a6ba645986e73dc48972ac5150a9e
MS_SON <alstmd89@gmail.com>
2020-06-27 20:00:49 +0900

    no message

--- Cephalon.py
+++ Cephalon.py
@@ -1,25 +1,21 @@
-import asyncio, discord
+import discord
 import os
 import urllib.request
 import json
-from discord.ext import commands
 
-token = os.environ["BOT_TOKEN"]
-bot = commands.Bot(command_prefix='!',status=discord.Status.online)
+client = discord.Client()
 
-@bot.event
+@client.event
 async def on_ready():
     print ("봇 시작")
 
-@bot.command()
-async def 명령어(ctx):
-    await ctx.send("도움말")
+@client.event
+async def on_message(message):
+    if message.content.startswith("!시터스"):
+        req = urllib.request.Request('https://api.warframestat.us/pc/cetusCycle', headers={'User-Agent': 'Mozilla/5.0'})
+        text_data = urllib.request.urlopen(req).read()
+        json_data = json.loads(text_data)
+        await message.channel.send(message.channel, json_data.get("shortString").replace("Day","낮").replace("Night","밤").replace("h","시간").replace("m","분").replace("s","초").replace("to","후"))
 
-@bot.command()
-async def 시터스(ctx):
-    req = urllib.request.Request('https://api.warframestat.us/pc/cetusCycle', headers={'User-Agent': 'Mozilla/5.0'})
-    text_data = urllib.request.urlopen(req).read()
-    json_data = json.loads(text_data)
-    await ctx.send(json_data.get("shortString").replace("Day","낮").replace("Night","밤").replace("h","시간").replace("m","분").replace("s","초").replace("to","후"))
-
-bot.run(token)
+token = os.environ["BOT_TOKEN"]
+client.run(token)
