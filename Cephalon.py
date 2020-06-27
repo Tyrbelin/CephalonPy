b951e3bbb3f17ced2f8627316fd673671ad18589
Tyrbelin <36834540+Tyrbelin@users.noreply.github.com>
2020-06-27 20:13:47 +0900

    Update Cephalon.py

--- Cephalon.py
+++ Cephalon.py
@@ -15,7 +15,7 @@ async def on_message(message):
         req = urllib.request.Request('https://api.warframestat.us/pc/cetusCycle', headers={'User-Agent': 'Mozilla/5.0'})
         text_data = urllib.request.urlopen(req).read()
         json_data = json.loads(text_data)
-        await message.channel.send(message.channel, json_data.get("shortString").replace("Day","낮").replace("Night","밤").replace("h","시간").replace("m","분").replace("s","초").replace("to","후"))
+        await client.send_message(message.channel, json_data.get("shortString").replace("Day","낮").replace("Night","밤").replace("h","시간").replace("m","분").replace("s","초").replace("to","후"))
 
 token = os.environ["BOT_TOKEN"]
 client.run(token)
