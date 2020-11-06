38ac6293b0cffc917004c1a0fe5dd375d52a1de5
Tyrbelin <36834540+Tyrbelin@users.noreply.github.com>
2020-11-07 02:03:41 +0900

    Update Cephalon.py

--- Cephalon.py
+++ Cephalon.py
@@ -11,11 +11,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("!시터스"):
-        req = urllib.request.Request('https://api.warframestat.us/pc/cetusCycle', headers={'User-Agent': 'Mozilla/5.0'})
+    if message.content.startswith("!몬스터"):
+        monster_name = message.content.split(' ')[1]
+        req = urllib.request.Request('http://mhw.gamedb.kr/apis/monsters/'+monster_name, headers={'User-Agent': 'Mozilla/5.0'})
         text_data = urllib.request.urlopen(req).read()
         json_data = json.loads(text_data)
-        await client.send_message(message.channel, json_data.get("shortString").replace("Day","낮").replace("Night","밤").replace("h","시간").replace("m","분").replace("s","초").replace("to","후"))
+        await client.send_message(message.channel, json_data)
 
 token = os.environ["BOT_TOKEN"]
 client.run(token)
